e0ef9f29d1a045094ca9a83d4cd6dfb2e610614d
Paul Backhouse <paulcbackhouse@gmail.com>
2020-06-14 12:59:01 +0100

    prettier.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import Ionic from "@ionic/vue";
-import App from "./App.vue";
-import "@ionic/core/css/core.css";
-import "@ionic/core/css/ionic.bundle.css";
+import Vue from 'vue';
+import Ionic from '@ionic/vue';
+import App from './App.vue';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
 
 Vue.config.productionTip = false;
 
@@ -10,4 +10,4 @@ Vue.use(Ionic);
 
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
