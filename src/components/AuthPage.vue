a71bc1cde6b0a87f1da1d175eb0e1ba9120cb46d
Paul Backhouse <paulcbackhouse@gmail.com>
2020-08-14 16:06:45 +0100

    Added loadingController to auth page.

--- src/components/AuthPage.vue
+++ src/components/AuthPage.vue
@@ -7,6 +7,15 @@
     </ion-header>
 
     <ion-content class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-label position="floating">Email</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
+      </ion-list>
+      <!-- <div class="ion-text-center">
+        <ion-spinner color="primary" v-if="isLoading"></ion-spinner>
+      </div> -->
       <ion-button color="primary" @click="onLogin">Login</ion-button>
     </ion-content>
   </ion-page>
@@ -14,10 +23,28 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isLoading: false
+    };
+  },
   methods: {
     onLogin() {
       this.$store.commit('login');
-      this.$router.push('/places/tabs/discover');
+      this.isLoading = true;
+      this.$ionic.loadingController
+        .create({
+          keyboardClose: true,
+          message: 'Logging in...'
+        })
+        .then(loadingEl => {
+          loadingEl.present();
+          setTimeout(() => {
+            this.isLoading = false;
+            loadingEl.dismiss();
+            this.$router.push('/places/tabs/discover');
+          }, 1500);
+        });
     }
   }
 };
