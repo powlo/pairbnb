6117025a0bc5a8db79408893ef51666aab8e1648
Paul Backhouse <paulcbackhouse@gmail.com>
2020-12-26 17:42:17 +0000

    224. Displaying a place preview.

--- src/components/LocationPicker.vue
+++ src/components/LocationPicker.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <ion-button color="primary" @click="onPickLocation">
+    <ion-spinner v-if="isLoading" color="primary"></ion-spinner>
+    <ion-img
+      v-if="!isLoading && selectedLocationImage"
+      :src="selectedLocationImage"
+      class="location-image"
+    ></ion-img>
+    <ion-button v-if="!isLoading && !selectedLocationImage" color="primary" @click="onPickLocation">
       <ion-icon name="map" slot="start"></ion-icon>
       <ion-label>Select Location</ion-label>
     </ion-button>
@@ -19,6 +25,12 @@ addIcons({
 });
 
 export default {
+  data() {
+    return {
+      isLoading: false,
+      selectedLocationImage: null
+    };
+  },
   methods: {
     onPickLocation() {
       this.$ionic.modalController
@@ -31,13 +43,24 @@ export default {
         })
         .then(modalData => {
           if (!modalData) throw Error('No data.');
-          return this.getaddress(modalData.data.lat, modalData.data.lng);
-        })
-        .then(address => {
-          console.log(address);
+
+          this.isLoading = true;
+          const pickedLocation = {
+            lat: modalData.data.lat,
+            lng: modalData.data.lng,
+            address: null,
+            staticMapImageUrl: null
+          };
+          this.getAddress(modalData.data.lat, modalData.data.lng).then(address => {
+            pickedLocation.address = address;
+          });
+          const staticMapImageUrl = this.getMapImage(modalData.data.lat, modalData.data.lng, 14);
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
         });
     },
-    getaddress(lat, lng) {
+    getAddress(lat, lng) {
       return fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`
       )
@@ -53,9 +76,32 @@ export default {
           }
           return geoData.results[0].formatted_address;
         });
+    },
+    getMapImage(lat, lng, zoom) {
+      return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:C%7C${lat},${lng}
+    &key=${environment.googleMapsAPIKey}`;
     }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.picker {
+  width: 30rem;
+  max-width: 80%;
+  height: 20rem;
+  max-height: 30vh;
+  border: 1px solid var(--ion-color-primary);
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.location-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
