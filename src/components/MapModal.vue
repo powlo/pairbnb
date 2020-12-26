5455b2807f188629a38b252ad4b4d5aecab1e230
Paul Backhouse <paulcbackhouse@gmail.com>
2020-12-26 13:34:08 +0000

    219. Adding the google maps SDK.

--- src/components/MapModal.vue
+++ src/components/MapModal.vue
@@ -18,10 +18,40 @@
 
 <script>
 export default {
+  created() {
+    this.getGoogleMaps().catch(err => {
+      console.error(err);
+    });
+  },
   methods: {
     onCancel() {
       // TODO: pass a data object to dismiss.
       this.$ionic.modalController.dismiss();
+    },
+    getGoogleMaps() {
+      // If googlemaps alread exists then return it.
+      const win = window;
+      const googleModule = win.google;
+      if (googleModule && googleModule.maps) {
+        return Promise.resolve(googleModule.maps);
+      }
+      return new Promise((resolve, reject) => {
+        // We use a promise because squirting stuff into the dom, that in turn
+        // makes http requests, takes time.
+        const script = document.createElement('script');
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=abc123';
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+        script.onload = () => {
+          const loadedGoogleModule = win.google;
+          if (loadedGoogleModule && loadedGoogleModule.maps) {
+            resolve(loadedGoogleModule.maps);
+          } else {
+            reject(Error('Google maps sdk not available'));
+          }
+        };
+      });
     }
   }
 };
